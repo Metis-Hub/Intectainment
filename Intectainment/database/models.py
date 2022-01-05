cd64e972387e6c7a198a7b730cd4d40d4f22d294
Tollpatsch314 <76736177+Tollpatsch314@users.noreply.github.com>
2022-01-05 16:16:39 +0100

    privilege & password nicht mehr unique

--- Intectainment/database/models.py
+++ Intectainment/database/models.py
@@ -1,11 +1,13 @@
 from Intectainment.app import db
 
 class User(db.Model):
+
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True, nullable=False)
-	password = db.Column(db.String(80), unique=True, nullable=False)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	privilege = db.Column(db.Integer, unique=True, nullable=True)
+
+	username	=	db.Column( db.String(80)	, unique=True	, nullable=False )
+	password	=	db.Column( db.String(80)	, unique=False	, nullable=False )
+	email		=	db.Column( db.String(120)	, unique=True	, nullable=False )
+	privilege	=	db.Column( db.Integer		, unique=False	, nullable=True	 )
 
 	def __repr__(self):
 		return '<User %r>' % self.username
