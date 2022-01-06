0df66e86a9a75ee032f476c605be4081a8aa076c
Bruno Hoffmann <0xbruno.hoffmann@gmail.com>
2022-01-06 14:22:49 +0100

    Fixed config issues

--- Intectainment/app.py
+++ Intectainment/app.py
@@ -2,21 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import configparser, os
 
-
+# init config
 config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
-print(config.sections())
-
+for section in ["Server", "Database", "Development"]:
+	if section not in config:
+		config.add_section(section)
 
 app = Flask(__name__, template_folder="./webpages/templates", static_folder="./webpages/static")
 
 # server configs
-app.config["SERVER_NAME"] = f"{(config.get('Server').get('server', 'localhost'))}:{(config['Server'].get('port') or '3000')}"
-app.config["SECRET_KEY"] = config.get('Server').get("secretKey", "replaceWhenDeployToDoThings")
+app.config["SERVER_NAME"] = f"{(config['Server'].get('server', fallback='localhost'))}:{(config['Server'].get('port', fallback='3000'))}"
+app.config["SECRET_KEY"] = config['Server'].get("secretKey", fallback="replaceWhenDeployToDoThings")
 
 ## init database
-app.config["SQLALCHEMY_DATABASE_URI"] = config.get("Database").get("URI") or "mysql+pymysql://root:@localhost/intectainment"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = (config.get("Development").get("deyMode")) == "yes" #dev state
+app.config["SQLALCHEMY_DATABASE_URI"] = config['Database'].get("URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = (config['Development'].get("deyMode")) == "yes" #dev state
 db = SQLAlchemy(app)
 # load tables
 import Intectainment.database.models
