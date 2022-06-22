20204167a40340534f7cad6ebcedd49acf1b7138
Robotix-00 <0xbruno.hoffmann@gmail.com>
2022-06-22 15:15:01 +0200

    changed setup order

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ import os
 import Intectainment.datamodels as dbm
 from Intectainment import db
 
+
+# create content subdirectories
+for dir in ["Intectainment/content/posts", "Intectainment/content/img"]:
+    path = os.path.join(os.path.dirname(__file__), dir)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 db.create_all()
 
 # create default super user
-if not dbm.User.query.filter_by(username = "Admin").first():
-	user = dbm.User()
-	user.permission = dbm.User.PERMISSION.ADMIN
-	user.username = "Admin"
-	user.changePassword("intectainment")
-	db.session.add(user)
-	db.session.commit()
-
-for dir in ["Intectainment/content/posts", "Intectainment/content/img"]:
-	path = os.path.join(os.path.dirname(__file__), dir)
-	if not os.path.exists(path):
-		os.makedirs(path)
+if not dbm.User.query.filter_by(username="Admin").first():
+    user = dbm.User()
+    user.permission = dbm.User.PERMISSION.ADMIN
+    user.username = "Admin"
+    user.changePassword("intectainment")
+    db.session.add(user)
+    db.session.commit()
