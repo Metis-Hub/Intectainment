f7b9780ec144293c98753101896a34ba05d45a4e
Bruno Hoffmann <69535212+Robotix-00@users.noreply.github.com>
2022-06-25 12:47:58 +0200

    Ldapisation (#38)
    * Changed Project Layout
    
    * And thats why you commit regulary
    
    * Removed custom timeout
    
    * Made it better
    
    * Completet LDAP Setup
    
    * Fixed indentation
    
    * I think i know what to do
    
    * Isolated the basics of user authentication [non working]
    
    * Removed legacy templates for user/admin interface
    
    * Removed user as a database class
    
    * Added fixed relations with old user class
    
    * Fixed Fav/Defav
    
    * Kinda fixed subscriptions
    
    * Changed ldapadmin https policy
    
    * Fixed subs and favs to work as real objects
    
    * Fixed Post-Creation
    
    * Removed User setup since it is deprecated
    
    * Change portlayout of docker-compose
    
    * changed ldap base dn
    
    * changed default port back to 80
    
    * Removed login notification on console

--- Intectainment/datamodels.py
+++ Intectainment/datamodels.py
@@ -1,270 +1,141 @@
-import os.path, datetime
-
 from Intectainment import db, app
+from Intectainment.ldapAuthentication import User
+
+import os.path, datetime
 from flask import session, url_for
-import bcrypt, threading, time, string, random
 
 
-ChannelCategory = db.Table('channelCategories',
-    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True),
-    db.Column('channel_id', db.Integer, db.ForeignKey('channel.id'), primary_key=True)
+ChannelCategory = db.Table(
+    "ChannelCategory",
+    db.Column(
+        "category_id", db.Integer, db.ForeignKey("category.id"), primary_key=True
+    ),
+    db.Column("channel_id", db.Integer, db.ForeignKey("channel.id"), primary_key=True),
 )
 
-Subscription = db.Table('subscribedChannels',
-    db.Column('channel_id', db.Integer, db.ForeignKey('channel.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
-)
 
-Favorites = db.Table('favoritePost',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-	db.Column('post_id', db.Integer, db.ForeignKey('post.id'))
-)
-
-class User(db.Model):
-	class PERMISSION:
-		GUEST = 0
-		USER = 10
-		MODERATOR = 100
-		ADMIN = 255
+class Subscription(db.Model):
+    user = db.Column(db.String(80), primary_key=True)
+    channel_id = db.Column(db.Integer, db.ForeignKey("channel.id"), primary_key=True)
 
 
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+class Favorites(db.Model):
+    user = db.Column(db.String(80), primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), primary_key=True)
 
-	username	  	= db.Column(db.String(80), unique=True, nullable=False)
-	displayname   	= db.Column(db.String(80), unique=True)
-	password	  	= db.Column(db.String(80), nullable=False)
-	permission	  	= db.Column(db.Integer, default=PERMISSION.USER)
-	icon_extension	= db.Column(db.String(4))
-	img_xPos		= db.Column(db.Integer, nullable=False, default=0)
-	img_yPos		= db.Column(db.Integer, nullable=False, default=0)
-	img_zoom		= db.Column(db.Integer, nullable=False, default=5500)
-
-	subscriptions 	= db.relationship("Channel", secondary=Subscription, backref="subscibers")
-	favoritePosts 	= db.relationship("Post", secondary=Favorites, backref="favUsers")
-
-	channels 		= db.relationship("Channel", backref="owner")
-	posts    		= db.relationship("Post", backref="owner")
-
-	def __init__(self, **kwargs):
-		super(User, self).__init__(**kwargs)
-		self.lastActive = time.time()
-
-	def __repr__(self):
-		return '<User %r>' % self.username
-
-	def reload(self):
-		db.session.add(self)
-		db.session.commit()
-		if "User" in session and session["User"] in User.activeUsers.keys():
-			User.activeUsers[session["User"]] = User.query.filter_by(id=self.id).first()
-
-
-	# Timeout management
-	TIMEOUT_TIME: int = 60 * 30
-	activeUsers: dict = dict()
-	@staticmethod
-	def resetTimeout():
-		if "User" in session:
-			if session["User"] in User.activeUsers:
-				user = User.activeUsers[session["User"]]
-				user.lastActive = time.time()
-
-	# login/logout utility
-	@staticmethod
-	def logIn(username: str, password: str) -> bool:
-		if "User" in session and session["User"] in User.activeUsers.keys():
-			#already logged in
-			return True
-		else:
-			user: User = User.query.filter_by(username=username).first()
-			if not user:
-				return False
-
-			if user.validatePassword(password):
-				if not user in User.activeUsers.values():
-					key: str = None
-					while not key or key in User.activeUsers:
-						key = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=40))
-
-					User.activeUsers[key] = user
-					session["User"] = key
-					return True
-				else:
-					return True
-		return False
-
-	@staticmethod
-	def logOut() -> None:
-		if "User" in session:
-			User.activeUsers.pop(session["User"], None)
-			session.pop("User", None)
-
-	@staticmethod
-	def isLoggedIn() -> bool:
-		return "User" in session and session["User"] in User.activeUsers.keys()
-
-	@staticmethod
-	def getCurrentUser():
-		if "User" in session:
-			if session["User"] in User.activeUsers:
-				return User.activeUsers[session["User"]]
-		return None
-			
-	def validatePassword(self, password: str) -> bool:
-		# This fixes a weird error, don't touch!!!
-		if isinstance(self.password, str):
-			return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
-		return bcrypt.checkpw(password.encode("utf-8"), self.password)
-
-	def changePassword(self, newPassword: str) -> None:
-		self.password = bcrypt.hashpw(newPassword.encode("utf-8"), bcrypt.gensalt())
-
-	def getName(self) -> str:
-		return self.displayname or self.username
-
-	def getFavoritePosts(self):
-		return self.favoritePosts
-
-	def getSubscriptions(self):
-		return self.subscriptions
-
-	def getProfileImagePath(self):
-		if self.icon_extension:
-			return url_for("display_image_", type="usr", filename=str(self.id) + "." + self.icon_extension)
-		return url_for("static", filename="default_usrimg.png")
 
 class Channel(db.Model):
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-	name			= db.Column(db.String(80), unique=True, nullable=False)
-	description		= db.Column(db.String(80), nullable=True)
-	owner_id		= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	icon_extension	= db.Column(db.String(4))
-	img_xPos		= db.Column(db.Integer, nullable=False, default=0)
-	img_yPos		= db.Column(db.Integer, nullable=False, default=0)
-	img_zoom		= db.Column(db.Integer, nullable=False, default=5500)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(80), nullable=True)
+    owner = db.Column(db.String(80), nullable=False)
+    icon_extension = db.Column(db.String(4))
+    img_xPos = db.Column(db.Integer, nullable=False, default=0)
+    img_yPos = db.Column(db.Integer, nullable=False, default=0)
+    img_zoom = db.Column(db.Integer, nullable=False, default=5500)
 
-	categories		= db.relationship("Category", secondary=ChannelCategory, backref="channels")
-	posts			= db.relationship("Post", backref="channel")
+    categories = db.relationship(
+        "Category", secondary=ChannelCategory, backref="channels"
+    )
+    posts = db.relationship("Post", backref="channel")
 
-	canModify = lambda self, user: user and (user.permission >= User.PERMISSION.MODERATOR or user.id == self.owner.id)
-	
-	def getProfileImagePath(self):
-		if self.icon_extension:
-			return url_for("display_image_", type="c", filename=str(self.id) + "." + self.icon_extension)
-		return url_for("static", filename="default_chimg.png")
+    canModify = lambda self, user: user and (
+        user.permission >= User.PERMISSION.MODERATOR or user.id == self.owner.id
+    )
+
+    def getProfileImagePath(self):
+        if self.icon_extension:
+            return url_for(
+                "display_image_",
+                type="c",
+                filename=str(self.id) + "." + self.icon_extension,
+            )
+        return url_for("static", filename="default_chimg.png")
+
 
 class Post(db.Model):
-	CONTENTDIRECTORY = "content/posts"
+    CONTENTDIRECTORY = "content/posts"
 
-	id 				= db.Column(db.Integer, primary_key=True, autoincrement=True)
-	creationDate 	= db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
-	modDate 		= db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    creationDate = db.Column(
+        db.DateTime, default=datetime.datetime.utcnow, nullable=False
+    )
+    modDate = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
 
-	channel_id 		= db.Column(db.Integer, db.ForeignKey('channel.id'), nullable=False)
-	owner_id 		= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    channel_id = db.Column(db.Integer, db.ForeignKey("channel.id"), nullable=False)
+    owner = db.Column(db.String(80), nullable=False)
 
+    def getContent(self):
+        """returns the content of post"""
+        try:
+            with open(self.getFilePath(), "r") as file:
+                return file.read()
+        except FileNotFoundError:
+            self.createFile()
+            return ""
 
-	def getContent(self):
-		"""returns the content of post"""
-		try:
-			with open(self.getFilePath(), "r") as file:
-				return file.read()
-		except FileNotFoundError:
-			self.createFile()
-			return ""
+    def getContentLines(self):
+        try:
+            with open(self.getFilePath(), "r") as file:
+                return file.readlines()
+        except FileNotFoundError:
+            self.createFile()
+            return []
 
-	def getContentLines(self):
-		try:
-			with open(self.getFilePath(), "r") as file:
-				return file.readlines()
-		except FileNotFoundError:
-			self.createFile()
-			return []
+    def setContent(self, content):
+        """sets the content of the post"""
+        with open(self.getFilePath(), "w", newline="\n") as file:
+            file.write(content)
 
-	def setContent(self, content):
-		"""sets the content of the post"""
-		with open(self.getFilePath(), "w", newline='\n') as file:
-			file.write(content)
+    def getFilePath(self):
+        """returns the path to the related post file"""
+        return os.path.join(
+            os.path.dirname(__file__),
+            self.CONTENTDIRECTORY,
+            f"{self.channel_id}-{self.id}.md",
+        )
 
-	def getFilePath(self):
-		"""returns the path to the related post file"""
-		return os.path.join(os.path.dirname(__file__), self.CONTENTDIRECTORY, f"{self.channel_id}-{self.id}.md")
+    def createFile(self):
+        if not os.path.isfile(self.getFilePath()):
+            with open(self.getFilePath(), "x") as f:
+                pass
 
-	def createFile(self):
-		if not os.path.isfile(self.getFilePath()):
-			with open(self.getFilePath(), "x") as f:
-				pass
+    canModify = lambda self, user: user and (
+        user.permission >= User.PERMISSION.MODERATOR or user.id == self.owner.id
+    )
 
-	canModify = lambda self, user: user and (user.permission >= User.PERMISSION.MODERATOR or user.id == self.owner.id)
+    @staticmethod
+    def new(channel_id, content, user=None):
+        """To create a basic Post"""
 
-	def addFav(self, user=None, commit=True):
+        if not user:
+            user = User.getCurrentUser()
 
-		if not user:
-			if currentUser := User.getCurrentUser():
-				user = User.query.filter_by(id=currentUser.id).first()
-			else:
-				return False
-		if not self in user.favoritePosts:
-			user.favoritePosts.append(self)
-			if commit: db.session.commit()
-			return True
-		return False
+        post = Post(channel_id=channel_id, owner=user.username)
+        """post has to be commited befor file can be created since the id isn't yet available"""
+        db.session.add(post)
+        db.session.commit()
 
-	def remFav(self, user=None, commit=True):
-		if not user:
-			if currentUser := User.getCurrentUser():
-				user = User.query.filter_by(id=currentUser.id).first()
-			else:
-				return False
-		if self in user.favoritePosts:
-			user.favoritePosts.remove(self)
-			if commit: db.session.commit()
-			return True
-		return False
+        post.createFile()
+        post.setContent(content)
 
-	@staticmethod
-	def new(channel_id, content, user=None):
-		"""To create a basic Post"""
+        return post
 
-		if not user:
-			user = User.getCurrentUser()
-
-		post = Post(channel_id=channel_id, owner=user)
-		"""post has to be commited befor file can be created since the id isn't yet available"""
-		db.session.add(post)
-		db.session.commit()
-
-		post.createFile()
-		post.setContent(content)
-
-		return post
-
-	def delete(self):
-		"""removes the db-object and the linked file"""
-		source_path = os.path.join(app.config["UPLOAD_FOLDER"], "c/", str(self.id) + "/")
-		if os.path.exists(source_path):
-			os.remove(source_path)
-		os.remove(self.getFilePath())
-		db.session.delete(self)
-
+    def delete(self):
+        """removes the db-object and the linked file"""
+        source_path = os.path.join(
+            app.config["UPLOAD_FOLDER"], "c/", str(self.id) + "/"
+        )
+        if os.path.exists(source_path):
+            os.remove(source_path)
+        os.remove(self.getFilePath())
+        db.session.delete(self)
 
 
 class Category(db.Model):
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column( db.String(80), unique=True, nullable=False )
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
 
-	def __repr__(self):
-		return self.name
-
-# init timeout check
-def checkUsers():
-	for key in User.activeUsers.keys():
-		user = User.activeUsers[key]
-		if time.time() - user.lastActive >= 60*20:
-			User.activeUsers.pop(key)
-		
-	time.sleep(60 * 1)
-afkCheckThread = threading.Thread(name="afkChecker", target=checkUsers)
-afkCheckThread.daemon = True
-afkCheckThread.start()
+    def __repr__(self):
+        return self.name
